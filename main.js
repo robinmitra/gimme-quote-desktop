fafa73331bc893f08359eed97d7ae3b7fcdd750c
Robin Mitra <robinmitra1@gmail.com>
2019-03-21 15:31:09 +0000

    Remove unnecessary code.

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ const menu = require('./src/menu/menu');
 const quoteService = require('./src/quoteService');
 const logger = require('./src/logger');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+// Keep a global reference of the tray object, if you don't, it will be closed automatically when
+// the JavaScript object is garbage collected.
 let tray;
 
 const initialise = () => quoteService.initialise().then(menu.initialise);
@@ -27,20 +26,3 @@ const createTray = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createTray);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-});
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createTray();
-  }
-});
